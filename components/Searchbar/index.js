ea6af7a78b260c78d7fd1133ff82e4a8ccd4b257
ngoctinvo <ngoctinvo2001@gmai.com>
2022-06-19 22:03:11 +0700

    tmp

--- components/Searchbar/index.js
+++ components/Searchbar/index.js
@@ -1,33 +1,33 @@
 import { useRouter } from "next/router";
 import { useState } from "react";
 import CInput from "react-composition-input";
-import Dictaphone from "../Dictaphone";
+// import Dictaphone from "../Dictaphone";
 import styles from "./styles.module.scss";
 
 const Searchbar = () => {
-    const [text, setText] = useState("");
+  const [text, setText] = useState("");
 
-    const router = useRouter();
+  const router = useRouter();
 
-    const handleChange = (e) => {
-        setText(e.target.value);
-    };
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
 
-    const handleSearch = (e) => {
-        e.preventDefault();
-        router.push(`/${text}`);
-    };
+  const handleSearch = (e) => {
+    e.preventDefault();
+    router.push(`/${text}`);
+  };
 
-    return (
-        <div className={styles.wrapper}>
-            <CInput onInputChange={handleChange} placeholder="Tìm kiếm từ..." />
-            <button onClick={handleSearch}>
-                <i className="fa-solid fa-magnifying-glass"></i>
-            </button>
+  return (
+    <div className={styles.wrapper}>
+      <CInput onInputChange={handleChange} placeholder="Tìm kiếm từ..." />
+      <button onClick={handleSearch}>
+        <i className="fa-solid fa-magnifying-glass"></i>
+      </button>
 
-            <Dictaphone></Dictaphone>
-        </div>
-    );
+      {/* <Dictaphone></Dictaphone> */}
+    </div>
+  );
 };
 
 export default Searchbar;
