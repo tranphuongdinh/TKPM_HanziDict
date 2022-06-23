10a5af694ee0f041ab31a494354bdc881e47079b
ngoctinvo <ngoctinvo2001@gmail.com>
2022-06-23 23:18:51 +0700

    update footer & header

--- containers/Footer/index.js
+++ containers/Footer/index.js
@@ -1,7 +1,28 @@
 import styles from "./styles.module.scss";
-
+import "font-awesome/css/font-awesome.min.css";
 const Footer = () => {
-    return <div className={styles.wrapper}>Footer</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div>
+        <h3>Information</h3>
+        <a>Home</a>
+        <a>About Us</a>
+        <a>Contribute</a>
+      </div>
+      <div>
+        <h3>Contact</h3>
+        <a href="https://www.hcmus.edu.vn/">
+          <i className="fa fa-university"></i>hcmus.edu.vn
+        </a>
+        <a href="https://www.facebook.com/">
+          <i className="fab fa-facebook-square"></i>www.facebook.com
+        </a>
+        <a href="mailto: hanzi@gmail.com">
+          <i className="fa fa-envelope"></i>hanzi@gmail.com
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- containers/Header/index.js
+++ containers/Header/index.js
@@ -1,7 +1,16 @@
 import styles from "./styles.module.scss";
-
+import Image from "next/image";
+import logo from "../../public/images/logocute.png";
 const Header = () => {
-    return <div className={styles.wrapper}>Header</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.logo}>
+        <Image src={logo} width="100px" height="80px" alt="" />
+      </div>
+      <div className={styles.navbar}> </div>
+      <div className={styles.user_control}></div>
+    </div>
+  );
 };
 
 export default Header;
