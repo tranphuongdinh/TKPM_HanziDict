9155e14f74bcde17d0e7f130c69dfa80eafc693e
tinvongoc-ct <tin.vongoc@chotot.vn>
2022-06-25 16:53:21 +0700

    update footer

--- containers/Footer/index.js
+++ containers/Footer/index.js
@@ -1,28 +1,40 @@
 import "font-awesome/css/font-awesome.min.css";
 import styles from "./styles.module.scss";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import HomeWorkIcon from "@mui/icons-material/HomeWork";
+import MailIcon from "@mui/icons-material/Mail";
+import { Container } from "@mui/material";
 const Footer = () => {
-    return (
-        <div className={styles.wrapper}>
-            <div>
-                <h3>Thông tin</h3>
-                <a>Tra cứu</a>
-                <a>Luyện viết</a>
-                <a>Đóng góp</a>
-            </div>
-            <div>
-                <h3>Liên hệ</h3>
-                <a href="https://www.hcmus.edu.vn/">
-                    <i className="fa fa-university"></i>hcmus.edu.vn
-                </a>
-                <a href="https://www.facebook.com/">
-                    <i className="fab fa-facebook-square"></i>www.facebook.com
-                </a>
-                <a href="mailto: hanzi@gmail.com">
-                    <i className="fa fa-envelope"></i>hanzi@gmail.com
-                </a>
-            </div>
+  return (
+    <div className={styles.wrapper}>
+      <Container maxWidth="xl" className={styles.wrapperContainer}>
+        <div>
+          <h3>Thông tin</h3>
+          <a>Tra cứu</a>
+          <a>Luyện viết</a>
+          <a>Đóng góp</a>
         </div>
-    );
+        <div>
+          <h3>Liên hệ</h3>
+          <a href="https://www.hcmus.edu.vn/">
+            <HomeWorkIcon
+              sx={{ fontSize: 30, lineHeight: 50 }}
+              className={styles.icon}
+            />
+            hcmus.edu.vn
+          </a>
+          <a href="https://www.facebook.com/">
+            <FacebookIcon sx={{ fontSize: 30 }} className={styles.icon} />
+            www.facebook.com
+          </a>
+          <a href="mailto: hanzi@gmail.com">
+            <MailIcon sx={{ fontSize: 30 }} className={styles.icon} />
+            hanzi@gmail.com
+          </a>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default Footer;
