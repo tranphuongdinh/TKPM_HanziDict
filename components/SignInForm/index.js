5ee9323492081e0458ead00e9f05a6a3386f8b78
ngoctinvo <ngoctinvo2001@gmail.com>
2022-06-26 23:16:06 +0700

    change login button style

--- components/SignInForm/index.js
+++ components/SignInForm/index.js
@@ -11,93 +11,93 @@ import LoadingScreen from "/components/LoadingScreen";
 import { AuthContext } from "/context/auth/auth.context";
 
 export default function SignInForm() {
-    const schema = yup
-        .object({
-            email: yup
-                .string()
-                .email("Email không hợp lệ")
-                .required("Trường này là bắt buộc"),
-            password: yup.string().required("Mật khẩu không được để trống"),
-        })
-        .required();
+  const schema = yup
+    .object({
+      email: yup
+        .string()
+        .email("Email không hợp lệ")
+        .required("Trường này là bắt buộc"),
+      password: yup.string().required("Mật khẩu không được để trống"),
+    })
+    .required();
 
-    const {
-        formState: { errors },
-        control,
-        handleSubmit,
-    } = useForm({ resolver: yupResolver(schema) });
+  const {
+    formState: { errors },
+    control,
+    handleSubmit,
+  } = useForm({ resolver: yupResolver(schema) });
 
-    const { authDispatch } = useContext(AuthContext);
+  const { authDispatch } = useContext(AuthContext);
 
-    const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    const handleSignIn = async (data) => {
-        setLoading(true);
-        const res = await getAuthClient().login(data);
-        if (res?.data?.message === "Successful.") {
-            toast.success("Đăng nhập thành công!");
-            authDispatch({
-                type: "LOGIN",
-                payload: res.data.data,
-            });
-            setLoading(false);
-            return;
-        }
-        toast.error(
-            res?.data?.message
-                ? `Có lỗi xảy ra: ${res.data.message}`
-                : "Email hoặc mật khẩu không đúng!"
-        );
-        setLoading(false);
-    };
-
-    return (
-        <Box
-            component="form"
-            noValidate
-            autoComplete="off"
-            onSubmit={handleSubmit((data) => handleSignIn(data))}
-        >
-            {loading && <LoadingScreen />}
-            <Box sx={{ display: "flex", flexDirection: "column", mt: 2 }}>
-                <Controller
-                    name="email"
-                    defaultValue=""
-                    control={control}
-                    render={({ field }) => (
-                        <TextField
-                            error={!!errors?.email}
-                            label="Email"
-                            helperText={errors?.email?.message}
-                            variant="standard"
-                            type="email"
-                            sx={{ mb: 2 }}
-                            {...field}
-                        />
-                    )}
-                />
-
-                <Controller
-                    name="password"
-                    defaultValue=""
-                    control={control}
-                    render={({ field }) => (
-                        <TextField
-                            error={!!errors?.password}
-                            label="Password"
-                            helperText={errors?.password?.message}
-                            variant="standard"
-                            type="password"
-                            {...field}
-                        />
-                    )}
-                />
-            </Box>
-            <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
-                <Button type="submit" variant="contained" sx={{ mt: 2 }}>
-                    Đăng nhập
-                </Button>
-            </Box>
-        </Box>
+  const handleSignIn = async (data) => {
+    setLoading(true);
+    const res = await getAuthClient().login(data);
+    if (res?.data?.message === "Successful.") {
+      toast.success("Đăng nhập thành công!");
+      authDispatch({
+        type: "LOGIN",
+        payload: res.data.data,
+      });
+      setLoading(false);
+      return;
+    }
+    toast.error(
+      res?.data?.message
+        ? `Có lỗi xảy ra: ${res.data.message}`
+        : "Email hoặc mật khẩu không đúng!"
     );
+    setLoading(false);
+  };
+
+  return (
+    <Box
+      component="form"
+      noValidate
+      autoComplete="off"
+      onSubmit={handleSubmit((data) => handleSignIn(data))}
+    >
+      {loading && <LoadingScreen />}
+      <Box sx={{ display: "flex", flexDirection: "column", mt: 2 }}>
+        <Controller
+          name="email"
+          defaultValue=""
+          control={control}
+          render={({ field }) => (
+            <TextField
+              error={!!errors?.email}
+              label="Email"
+              helperText={errors?.email?.message}
+              variant="standard"
+              type="email"
+              sx={{ mb: 2 }}
+              {...field}
+            />
+          )}
+        />
+
+        <Controller
+          name="password"
+          defaultValue=""
+          control={control}
+          render={({ field }) => (
+            <TextField
+              error={!!errors?.password}
+              label="Password"
+              helperText={errors?.password?.message}
+              variant="standard"
+              type="password"
+              {...field}
+            />
+          )}
+        />
+      </Box>
+      <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+        <Button type="submit" variant="contained" sx={{ mt: 2 }}>
+          Đăng nhập
+        </Button>
+      </Box>
+    </Box>
+  );
 }
--- containers/Header/index.js
+++ containers/Header/index.js
@@ -20,248 +20,234 @@ import { AuthContext } from "/context/auth/auth.context";
 import logo from "/public/images/logocute.png";
 
 const Header = () => {
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-    const handleOpenNavMenu = (event) => {
-        setAnchorElNav(event.currentTarget);
-    };
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
-    };
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-    const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
-    };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-    };
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
-    const {
-        authState: { isAuthenticated },
-        authDispatch,
-    } = React.useContext(AuthContext);
+  const {
+    authState: { isAuthenticated },
+    authDispatch,
+  } = React.useContext(AuthContext);
 
-    const [showAuth, setShowAuth] = React.useState(false);
+  const [showAuth, setShowAuth] = React.useState(false);
 
-    const handleShowAuth = (show) => {
-        setShowAuth(show);
-    };
+  const handleShowAuth = (show) => {
+    setShowAuth(show);
+  };
 
-    const router = useRouter();
+  const router = useRouter();
 
-    const pages = [
-        {
-            label: "Tra cứu",
-            url: "/",
-        },
-        {
-            label: "Luyện viết",
-            url: "/writing",
-        },
-        { label: "Đóng góp", url: "/contribution" },
-    ];
-    const settings = [
-        {
-            label: "Account",
-            event: () => {
-                router.push("/account");
-            },
-        },
-        {
-            label: "Logout",
-            event: () => {
-                authDispatch({
-                    type: "LOGOUT",
-                });
-                setShowAuth(false);
-            },
-        },
-    ];
+  const pages = [
+    {
+      label: "Tra cứu",
+      url: "/",
+    },
+    {
+      label: "Luyện viết",
+      url: "/writing",
+    },
+    { label: "Đóng góp", url: "/contribution" },
+  ];
+  const settings = [
+    {
+      label: "Account",
+      event: () => {
+        router.push("/account");
+      },
+    },
+    {
+      label: "Logout",
+      event: () => {
+        authDispatch({
+          type: "LOGOUT",
+        });
+        setShowAuth(false);
+      },
+    },
+  ];
 
-    return (
-        <AppBar position="static" style={{ backgroundColor: "#0091a7" }}>
-            <Container maxWidth="lg">
-                <Toolbar disableGutters>
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="a"
-                        href="/"
-                        sx={{
-                            mr: 2,
-                            display: { xs: "none", md: "flex" },
-                            fontFamily: "monospace",
-                            fontWeight: 700,
-                            letterSpacing: ".3rem",
-                            color: "inherit",
-                            textDecoration: "none",
-                        }}
-                    >
-                        <Image src={logo} width={40} height={40} alt="logo" />
-                    </Typography>
+  return (
+    <AppBar position="static" style={{ backgroundColor: "#0091a7" }}>
+      <Container maxWidth="lg">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            <Image src={logo} width={40} height={40} alt="logo" />
+          </Typography>
 
-                    <Box
-                        sx={{
-                            flexGrow: 1,
-                            display: { xs: "flex", md: "none" },
-                        }}
-                    >
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleOpenNavMenu}
-                            color="inherit"
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorElNav}
-                            anchorOrigin={{
-                                vertical: "bottom",
-                                horizontal: "left",
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: "top",
-                                horizontal: "left",
-                            }}
-                            open={Boolean(anchorElNav)}
-                            onClose={handleCloseNavMenu}
-                            sx={{
-                                display: { xs: "block", md: "none" },
-                            }}
-                        >
-                            {pages.map((page) => (
-                                <Link href={page.url} key={page.label}>
-                                    <MenuItem onClick={handleCloseNavMenu}>
-                                        <Typography textAlign="center">
-                                            {page.label}
-                                        </Typography>
-                                    </MenuItem>
-                                </Link>
-                            ))}
-                        </Menu>
-                    </Box>
-                    <Typography
-                        variant="h5"
-                        noWrap
-                        component="a"
-                        href=""
-                        sx={{
-                            mr: 2,
-                            display: { xs: "flex", md: "none" },
-                            flexGrow: 1,
-                            fontFamily: "monospace",
-                            fontWeight: 700,
-                            letterSpacing: ".3rem",
-                            color: "inherit",
-                            textDecoration: "none",
-                        }}
-                    >
-                        <Image src={logo} width={40} height={40} alt="logo" />
-                    </Typography>
-                    <Box
-                        sx={{
-                            flexGrow: 1,
-                            display: { xs: "none", md: "flex" },
-                            ml: 2,
-                        }}
-                    >
-                        {pages.map((page) => (
-                            <Link href={page.url} key={page.label}>
-                                <Button
-                                    onClick={handleCloseNavMenu}
-                                    sx={{
-                                        my: 2,
-                                        mr: 2,
-                                        color: "white",
-                                        display: "block",
-                                        position: "relative",
-                                    }}
-                                    className={
-                                        router.asPath === page.url
-                                            ? styles.active
-                                            : ""
-                                    }
-                                >
-                                    {page.label}
-                                </Button>
-                            </Link>
-                        ))}
-                    </Box>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "flex", md: "none" },
+            }}
+          >
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <Link href={page.url} key={page.label}>
+                  <MenuItem onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{page.label}</Typography>
+                  </MenuItem>
+                </Link>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            <Image src={logo} width={40} height={40} alt="logo" />
+          </Typography>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", md: "flex" },
+              ml: 2,
+            }}
+          >
+            {pages.map((page) => (
+              <Link href={page.url} key={page.label}>
+                <Button
+                  onClick={handleCloseNavMenu}
+                  sx={{
+                    my: 2,
+                    mr: 2,
+                    color: "white",
+                    display: "block",
+                    position: "relative",
+                  }}
+                  className={router.asPath === page.url ? styles.active : ""}
+                >
+                  {page.label}
+                </Button>
+              </Link>
+            ))}
+          </Box>
 
-                    {isAuthenticated ? (
-                        <Box sx={{ flexGrow: 0 }}>
-                            <Tooltip title="Open settings">
-                                <IconButton
-                                    onClick={handleOpenUserMenu}
-                                    sx={{ p: 0 }}
-                                >
-                                    <Avatar
-                                        alt="Remy Sharp"
-                                        src="/static/images/avatar/2.jpg"
-                                    />
-                                </IconButton>
-                            </Tooltip>
-                            <Menu
-                                sx={{ mt: "45px" }}
-                                id="menu-appbar"
-                                anchorEl={anchorElUser}
-                                anchorOrigin={{
-                                    vertical: "top",
-                                    horizontal: "right",
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: "top",
-                                    horizontal: "right",
-                                }}
-                                open={Boolean(anchorElUser)}
-                                onClose={handleCloseUserMenu}
-                            >
-                                {settings.map((setting) => (
-                                    <MenuItem
-                                        key={setting}
-                                        onClick={() => {
-                                            setting.event();
-                                            handleCloseUserMenu();
-                                        }}
-                                    >
-                                        <Typography textAlign="center">
-                                            {setting.label}
-                                        </Typography>
-                                    </MenuItem>
-                                ))}
-                            </Menu>
-                        </Box>
-                    ) : (
-                        <Box>
-                            <Button
-                                variant="contained"
-                                color="primary"
-                                sx={{ mr: 2 }}
-                                onClick={() => {
-                                    handleShowAuth(true);
-                                }}
-                            >
-                                Đăng nhập
-                            </Button>
-                            {showAuth && (
-                                <AuthForm
-                                    handleShowAuth={handleShowAuth}
-                                    show={showAuth}
-                                ></AuthForm>
-                            )}
-                        </Box>
-                    )}
-                </Toolbar>
-            </Container>
-        </AppBar>
-    );
+          {isAuthenticated ? (
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem
+                    key={setting}
+                    onClick={() => {
+                      setting.event();
+                      handleCloseUserMenu();
+                    }}
+                  >
+                    <Typography textAlign="center">{setting.label}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          ) : (
+            <Box>
+              <Button
+                variant="contained"
+                className={styles.btnLogin}
+                sx={{ mr: 2 }}
+                onClick={() => {
+                  handleShowAuth(true);
+                }}
+              >
+                Đăng nhập
+              </Button>
+              {showAuth && (
+                <AuthForm
+                  handleShowAuth={handleShowAuth}
+                  show={showAuth}
+                ></AuthForm>
+              )}
+            </Box>
+          )}
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
 export default Header;
