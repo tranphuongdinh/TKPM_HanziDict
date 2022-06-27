2f93aa149259bb177a7f8dfa60cf57b800e4a88e
ngoctinvo <ngoctinvo2001@gmail.com>
2022-06-27 18:00:32 +0700

    navigate <a> in footer

--- containers/Footer/index.js
+++ containers/Footer/index.js
@@ -4,43 +4,58 @@ import MailIcon from "@mui/icons-material/Mail";
 import { Container } from "@mui/material";
 import "font-awesome/css/font-awesome.min.css";
 import styles from "./styles.module.scss";
+import Link from "next/link";
+import Typography from "@mui/material/Typography";
+
 const Footer = () => {
-    return (
-        <div className={styles.wrapper}>
-            <Container maxWidth="lg" className={styles.wrapperContainer}>
-                <div>
-                    <h3>Thông tin</h3>
-                    <a>Tra cứu</a>
-                    <a>Luyện viết</a>
-                    <a>Đóng góp</a>
-                </div>
-                <div>
-                    <h3>Liên hệ</h3>
-                    <a href="https://www.hcmus.edu.vn/">
-                        <HomeWorkIcon
-                            sx={{ fontSize: 30, lineHeight: 50 }}
-                            className={styles.icon}
-                        />
-                        hcmus.edu.vn
-                    </a>
-                    <a href="https://www.facebook.com/">
-                        <FacebookIcon
-                            sx={{ fontSize: 30 }}
-                            className={styles.icon}
-                        />
-                        www.facebook.com
-                    </a>
-                    <a href="mailto: hanzi@gmail.com">
-                        <MailIcon
-                            sx={{ fontSize: 30 }}
-                            className={styles.icon}
-                        />
-                        hanzi@gmail.com
-                    </a>
-                </div>
-            </Container>
+  const pages = [
+    {
+      label: "Tra cứu",
+      url: "/",
+    },
+    {
+      label: "Luyện viết",
+      url: "/writing",
+    },
+    { label: "Đóng góp", url: "/contribution" },
+  ];
+  return (
+    <div className={styles.wrapper}>
+      <Container maxWidth="lg" className={styles.wrapperContainer}>
+        <div>
+          <h3>Thông tin</h3>
+          <Link href="/">
+            <a>Tra cứu</a>
+          </Link>
+          <Link href="/writing">
+            <a>Luyện viết</a>
+          </Link>
+
+          <Link href="/contribution">
+            <a>Đóng góp</a>
+          </Link>
         </div>
-    );
+        <div>
+          <h3>Liên hệ</h3>
+          <a href="https://www.hcmus.edu.vn/">
+            <HomeWorkIcon
+              sx={{ fontSize: 30, lineHeight: 50 }}
+              className={styles.icon}
+            />
+            hcmus.edu.vn
+          </a>
+          <a href="https://www.facebook.com/">
+            <FacebookIcon sx={{ fontSize: 30 }} className={styles.icon} />
+            www.facebook.com
+          </a>
+          <a href="mailto: hanzi@gmail.com">
+            <MailIcon sx={{ fontSize: 30 }} className={styles.icon} />
+            hanzi@gmail.com
+          </a>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default Footer;
