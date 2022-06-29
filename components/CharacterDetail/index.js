5ed2127eec608c4c2a41569ad663dfec19911f42
ngoctinvo <ngoctinvo2001@gmail.com>
2022-06-29 16:41:40 +0700

    add character detail UI

---
+++ components/CharacterDetail/index.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import styles from "./styles.module.scss";
+import Card from "@mui/material/Card";
+
+export const CharacterDetail = ({ data }) => {
+  const character = data.character;
+  const [count, setCount] = useState(1);
+  const [image, setImage] = useState({ src: character.img[0], index: 0 });
+
+  const num = character.img.length;
+
+  const changeImage = () => {
+    setImage((state) => ({
+      src: character.img[state.index],
+      index: (state.index + 1) % num,
+    }));
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      changeImage();
+      console.log(image.index);
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <div className={styles.wapper}>
+      <h1>Chi tiết từ {character.chineseName}</h1>
+      <Card className={styles.basicInfoWapper}>
+        <div className={styles.chineseCharacter}>{character.chineseName}</div>
+        <div className={styles.basicInfo}>
+          <div className={styles.itemWapper}>
+            <div className={styles.item}>
+              <span>Pinyin:</span>
+              <span> {character.pinyin}</span>
+            </div>
+            <div className={styles.item}>
+              <span>Hán việt:</span>
+              <span> {character.sino}</span>
+            </div>
+          </div>
+
+          <div className={styles.item}>
+            <span>Nghĩa:</span>
+            <span> {character.mean}</span>
+          </div>
+          <div className={styles.item}>
+            <span>Nghĩa mở rộng:</span>
+            <span> {character.deepMean}</span>
+          </div>
+        </div>
+      </Card>
+      <Card className={styles.expandInfoWapper}>
+        <div className={styles.animation}>
+          <img src={image.src} alt="" />
+        </div>
+
+        <div className={styles.explain}>
+          <span>Giải thích:</span>
+          <span> {character.explain}</span>
+        </div>
+      </Card>
+    </div>
+  );
+};
--- pages/characters/[id].js
+++ pages/characters/[id].js
@@ -1,22 +1,23 @@
 import { getCharactersClient } from "/apis/getCharactersClient";
 import Template from "/containers/Template";
+import { CharacterDetail } from "../../components/CharacterDetail";
 
 const SearchResult = ({ data }) => {
-    return (
-        <Template title="Search result | Hanzi Dict">
-            {JSON.stringify(data)}
-        </Template>
-    );
+  return (
+    <Template title="Search result | Hanzi Dict">
+      <CharacterDetail data={data} />
+    </Template>
+  );
 };
 
 export async function getServerSideProps(ctx) {
-    const { id } = ctx.query;
-    const data = await getCharactersClient().getCharById(id);
-    return {
-        props: {
-            data: data,
-        },
-    };
+  const { id } = ctx.query;
+  const data = await getCharactersClient().getCharById(id);
+  return {
+    props: {
+      data: data,
+    },
+  };
 }
 
 export default SearchResult;
