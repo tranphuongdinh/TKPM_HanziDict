739f65d3eb9b02e40c2188b440109edc1a55ad7d
TranPhuongDinh <dinhblade123@gmail.com>
2022-06-29 10:06:00 +0700

    update management page

--- components/CharsTable/index.js
+++ components/CharsTable/index.js
@@ -6,18 +6,59 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 import { v4 as uuidv4 } from "uuid";
+import { getCharactersClient } from "../../apis/getCharactersClient";
+import LoadingScreen from "../../components/LoadingScreen";
 
-export default function CharsTable({ data }) {
+export default function CharsTable({ data, type }) {
     const [charsData, setCharsData] = useState(data || []);
+    const [loading, setLoading] = useState(false);
+    const router = useRouter();
 
     useEffect(() => {
         setCharsData(data);
     }, [data]);
 
+    const handleUpdate = async (id, action) => {
+        setLoading(true);
+        if (action === "DELETE") {
+            const res = await getCharactersClient().deleteChar(id);
+            if (res?.success) {
+                toast.success("Xóa thành công");
+                const newCharsData = charsData.filter((char) => char.id !== id);
+                setCharsData(newCharsData);
+            }
+        }
+        if (action === "ACTIVATE") {
+            const res = await getCharactersClient().activateChar(id);
+            if (res?.success) {
+                toast.success("Kích hoạt thành công");
+                const newCharsData = charsData.filter((char) => char.id !== id);
+                setCharsData(newCharsData);
+            }
+        }
+
+        if (action === "DEACTIVATE") {
+            const res = await getCharactersClient().updateChar(id, {
+                isActive: false,
+            });
+            if (res?.success) {
+                toast.success("Hủy kích hoạt thành công");
+                const newCharsData = charsData.filter((char) => char.id !== id);
+                setCharsData(newCharsData);
+            }
+        }
+
+        setLoading(false);
+        router.reload();
+    };
+
     return (
         <TableContainer component={Paper}>
+            {loading && <LoadingScreen />}
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 <TableHead>
                     <TableRow>
@@ -51,8 +92,37 @@ export default function CharsTable({ data }) {
                                     : "Chưa kích hoạt"}
                             </TableCell>
                             <TableCell align="center">
-                                <Button variant="contained">Kích hoạt</Button>
-                                <Button variant="contained">Xóa</Button>
+                                {type === "ACTIVATE" ? (
+                                    <Button
+                                        variant="contained"
+                                        onClick={() => {
+                                            handleUpdate(
+                                                char._id,
+                                                "DEACTIVATE"
+                                            );
+                                        }}
+                                    >
+                                        Hủy kích hoạt
+                                    </Button>
+                                ) : (
+                                    <Button
+                                        variant="contained"
+                                        onClick={() => {
+                                            handleUpdate(char._id, "ACTIVATE");
+                                        }}
+                                    >
+                                        Kích hoạt
+                                    </Button>
+                                )}
+                                <Button
+                                    variant="contained"
+                                    sx={{ ml: 2 }}
+                                    onClick={() => {
+                                        handleUpdate(char._id, "DELETE");
+                                    }}
+                                >
+                                    Xóa
+                                </Button>
                             </TableCell>
                         </TableRow>
                     ))}
