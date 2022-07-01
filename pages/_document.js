a351395d5814a0114c8f7cff26a00776fa806d3d
TranPhuongDinh <dinhblade123@gmail.com>
2022-07-01 12:22:12 +0700

    add meta tags for SEO

--- pages/_document.js
+++ pages/_document.js
@@ -31,6 +31,49 @@ export default class CustomDocument extends Document {
         return (
             <Html>
                 <Head>
+                    <meta name="title" content="Hanzi Dict" />
+                    <meta
+                        name="description"
+                        content={`Với mong muốn có thể mang đến một công cụ tiện lợi, giúp các
+                    bạn đang theo học tiếng Trung có thể nhớ được Hán tự theo
+                    một cách trực quan nhất, dễ hiểu nhất, chúng mình đã mang
+                    đến một bộ từ điển "lạ lùng" chưa từng có. Hanzi Dict tập
+                    trung vào việc giải thích ý nghĩa của từng Hán tự, xem nó
+                    được tượng hình như thế nào, được ghép từ các thành phần ra
+                    sao, dùng hình ảnh để minh họa, từ đó giúp bạn học hình dung
+                    được hình ảnh tượng trưng, hiểu được ý nghĩa của Hán tự, ghi
+                    nhớ một cách tự nhiên mà không cần phải "học vẹt".`}
+                    />
+                    <meta name="robots" content="noodp,index,follow" />
+                    <meta name="revisit-after" content="1 days" />
+                    <meta httpEquiv="content-language" content="vi" />
+                    <meta property="og:title" content="Hanzi Dict" />
+                    <meta
+                        property="og:description"
+                        content={`Với mong muốn có thể mang đến một công cụ tiện lợi, giúp các
+                    bạn đang theo học tiếng Trung có thể nhớ được Hán tự theo
+                    một cách trực quan nhất, dễ hiểu nhất, chúng mình đã mang
+                    đến một bộ từ điển "lạ lùng" chưa từng có. Hanzi Dict tập
+                    trung vào việc giải thích ý nghĩa của từng Hán tự, xem nó
+                    được tượng hình như thế nào, được ghép từ các thành phần ra
+                    sao, dùng hình ảnh để minh họa, từ đó giúp bạn học hình dung
+                    được hình ảnh tượng trưng, hiểu được ý nghĩa của Hán tự, ghi
+                    nhớ một cách tự nhiên mà không cần phải "học vẹt".`}
+                    />
+                    <meta property="og:type" content="website" />
+                    <meta
+                        property="og:url"
+                        content="https://tkpm-hanzi-dict.herokuapp.com/"
+                    />
+                    <meta
+                        property="og:image"
+                        content="https://tkpm-hanzi-dict.herokuapp.com/images/logocute.png"
+                    />
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1"
+                    />
+
                     <link
                         rel="stylesheet"
                         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
